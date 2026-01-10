6b8d17d5d19997ba1eb34c952b6602785f045689
NicoleFernandes11 <A041771@umaia.pt>
2026-01-10 19:46:57 +0000

    JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,29 @@
-// JS do TrabalhoTW
+// Ler ficheiro XML e mostrar recursos na tabela
+fetch("../../dados.xml")
+  .then(response => response.text())
+  .then(texto => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(texto, "text/xml");
+
+    const recursos = xmlDoc.getElementsByTagName("recurso");
+    const tabela = document.getElementById("corpo-tabela");
+
+    for (let i = 0; i < recursos.length; i++) {
+      const id = recursos[i].getAttribute("id");
+      const tipo = recursos[i].getElementsByTagName("tipo")[0].textContent;
+      const nome = recursos[i].getElementsByTagName("nome")[0].textContent;
+      const uso = recursos[i].getElementsByTagName("uso")[0].textContent;
+
+      const linha = document.createElement("tr");
+      linha.innerHTML = `
+        <td>${id}</td>
+        <td>${tipo}</td>
+        <td>${nome}</td>
+        <td>${uso}</td>
+      `;
+
+      tabela.appendChild(linha);
+    }
+  });
+
+
